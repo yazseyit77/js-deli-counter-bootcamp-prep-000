b2f08bf0b6d3a38929521f4457bf32c88fe914b1
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-07-24 13:18:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function takeANumber(katzDeli, name) {
 
 function nowServing(katzDeli){
   for(var i = 0; i < katzDeli.length; i++){
-    line.push(` `+[i+1]+`. `  + katzDeli[i])
+
   }
   if(katzDeli.length === 0){
     return "There is nobody waiting to be served!";
