a0a17e9acc54ffb3dfc6519f518e6fad8f36c2e3
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-07-24 13:14:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ function takeANumber(katzDeli, name) {
 
 function nowServing(katzDeli){
   for(var i = 0; i < katzDeli.length; i++){
+    if(katzDeli.length; < 1){
+      return "There is nobody waiting to be served!";
+    } else{
+      return (`Currently serving ${katzDeli.shift()}.`);
+    }
   }
-  if(katzDeli.length; < 1){
-    return "There is nobody waiting to be served!";
-  } else{
-    return (`Currently serving ${katzDeli.shift()}.`);
-  }
+
 }
