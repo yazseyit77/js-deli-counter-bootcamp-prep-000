30e747a5eee2593b7f7acdeb4e9a59897f94d683
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-07-24 13:26:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function currentLine(katzDeliLine){
     line.push(` `+[i+1]+`. `  + katzDeliLine[i])
   }
   if(katzDeliLine.length === 0){
-    return "The line is currently empty!";
+    return "The line is currently empty.";
   } else{
     return (`The line is currently:` + line);
   }
