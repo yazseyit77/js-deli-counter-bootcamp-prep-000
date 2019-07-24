17c118a987405e9ee08c3fd935a535965cc0be9f
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-07-24 12:59:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var katzDeliLine = [];
 function takeANumber(currentLine, name){
   var array = [];
   for(var i = 0; i < name.length; i++){
-      array.push('Welcome, ' + name[i] + '. ' + 'You are number ' + currentLine[i] + ' in line.');
+      array.push('Welcome, ' + name[i] + '. You are number ' + currentLine[i] + ' in line.');
   }
   return array;
 }
